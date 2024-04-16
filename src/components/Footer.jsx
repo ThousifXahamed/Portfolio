01e03896d3f07d21da10ab9b8824154edb3a7b62
ThousifXahamed <119498330+ThousifXahamed@users.noreply.github.com>
2024-04-17 01:32:52 +0530

    Add files via upload
    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <>
+      <div className="px-2 py-2">
+        <div className="flex items-center justify-center text-center px-7 py-7 bg-gray-100 rounded-lg">
+          <p className="w-full">
+          © 2024 Portfolio – React Template by <a href="https://github.com/ThousifXahamed" className="font-medium underline" >ahamed</a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,11 @@
+import Projects from "../components/Projects";
+
+export default function Home() {
+    return (
+        <>
+         <Projects>
+          
+         </Projects>
+        </>
+    )
+}
